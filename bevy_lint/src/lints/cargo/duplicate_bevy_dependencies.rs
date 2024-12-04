a7983a35ef8eaf59360d1dcf7b30347af7d8e37b
DaAlbrecht <dadidi42@gmail.com>
2024-12-04 22:49:36 +0100

    feat: get span to correct crate in cargo.toml

--- bevy_lint/src/lints/cargo/duplicate_bevy_dependencies.rs
+++ bevy_lint/src/lints/cargo/duplicate_bevy_dependencies.rs
@@ -5,11 +5,18 @@
 //! When different third party crates use incompatible versions of Bevy, it can lead to confusing
 //! errors and type incompatibilities.
 
-use std::path::Path;
+use std::{path::Path, str::FromStr};
 
-use crate::{declare_bevy_lint, lints::cargo::toml_span};
-use cargo_metadata::{semver::VersionReq, Metadata, MetadataCommand, Package};
+use crate::{
+    declare_bevy_lint,
+    lints::cargo::{toml_span, CargoToml},
+};
+use cargo_metadata::{
+    semver::{Error, Version, VersionReq},
+    Metadata, MetadataCommand, Package,
+};
 use clippy_utils::{diagnostics::span_lint, find_crates, sym};
+use rustc_hir::def_id::LOCAL_CRATE;
 use rustc_lint::{LateContext, LateLintPass};
 use rustc_session::impl_lint_pass;
 use rustc_span::Symbol;
@@ -59,6 +66,8 @@ impl<'tcx> LateLintPass<'tcx> for DuplicateBevyDependencies {
 }
 
 fn check(cx: &LateContext<'_>, metadata: &Metadata) {
+    let local_name = cx.tcx.crate_name(LOCAL_CRATE);
+
     let file = cx
         .tcx
         .sess
@@ -68,43 +77,78 @@ fn check(cx: &LateContext<'_>, metadata: &Metadata) {
 
     let cargo_src = file.src.as_deref();
 
-    let toml_schmomel = toml::from_str::<String>(cargo_src.unwrap());
+    let cargo_toml = toml::from_str::<CargoToml>(cargo_src.unwrap()).unwrap();
 
-    dbg!(toml_schmomel);
+    let target_version = get_version_from_toml(
+        cargo_toml
+            .dependencies
+            .get("bevy")
+            .unwrap()
+            .as_ref()
+            .clone(),
+    )
+    .unwrap();
 
-    let bevy_crate = metadata
-        .packages
-        .iter()
-        .filter(|package| package.name == "bevy")
-        .cloned()
-        .collect::<Vec<Package>>();
+    dbg!(&target_version);
 
     let bevy_dependents = metadata
         .packages
         .iter()
-        .map(|package| {
+        .flat_map(|package| {
             package
                 .dependencies
                 .iter()
                 .filter_map(|dependency| match dependency.name.as_str() {
-                    "bevy" => Some((package.name.as_str(), dependency.req.clone())),
+                    "bevy" => {
+                        if package.name.as_str() == local_name.as_str() {
+                            return None;
+                        }
+                        Some((package.name.as_str(), dependency.req.clone()))
+                    }
                     _ => None,
                 })
                 .collect::<Vec<(&str, VersionReq)>>()
         })
-        .flatten()
         .collect::<Vec<(&str, VersionReq)>>();
 
-    println!("bevy_dependents: {:?}", bevy_dependents);
+    let incoherent_bevy_dependents = bevy_dependents
+        .iter()
+        .filter(|(_, dependent_version)| dependent_version.matches(&target_version))
+        .cloned()
+        .collect::<Vec<(&str, VersionReq)>>();
 
-    bevy_crate.iter().for_each(|bevy| {
-        println!("version: {}", bevy.version);
-    });
+    incoherent_bevy_dependents
+        .iter()
+        .for_each(|incoherent_version| {
+            dbg!(cargo_toml.dependencies.get(incoherent_version.0));
+            let cargo_toml_reference = cargo_toml
+                .dependencies
+                .get(incoherent_version.0)
+                .expect("crate to be present");
+            span_lint(
+                cx,
+                DUPLICATE_BEVY_DEPENDENCIES.lint,
+                toml_span(cargo_toml_reference.span(), &file),
+                format!(
+                    "Mismatching versions of `bevy` found, expected crate to be using bevy version: {}",
+                    target_version.to_string()
+                ),
+            );
+        });
+}
 
-    span_lint(
-        cx,
-        DUPLICATE_BEVY_DEPENDENCIES.lint,
-        toml_span(&file),
-        "Multiple versions of `bevy` found",
-    );
+fn get_version_from_toml(table: toml::Value) -> Result<Version, Error> {
+    // TODO: make this more robust, this fails if someone uses version ranges for bevy
+    match table {
+        toml::Value::String(version) => Version::from_str(version.as_str()),
+        toml::Value::Table(map) => {
+            let version = map.get("version").expect("version field is required");
+            Version::from_str(
+                version
+                    .as_str()
+                    .expect("version field is required to be a string"),
+            )
+        }
+        _ => panic!("impossible to hit"),
+    }
 }
--- bevy_lint/src/lints/cargo/mod.rs
+++ bevy_lint/src/lints/cargo/mod.rs
@@ -1,9 +1,21 @@
-use rustc_span::{SourceFile, Span, SyntaxContext};
+use std::{collections::BTreeMap, ops::Range};
+
+use rustc_span::{BytePos, Pos, SourceFile, Span, SyntaxContext};
+use serde::Deserialize;
+use toml::Spanned;
 
 pub mod duplicate_bevy_dependencies;
 
-pub(crate) fn toml_span(file: &SourceFile) -> Span {
-    Span::new(file.start_pos, file.start_pos, SyntaxContext::root(), None)
+pub(crate) fn toml_span(range: Range<usize>, file: &SourceFile) -> Span {
+    Span::new(
+        file.start_pos + BytePos::from_usize(range.start),
+        file.start_pos + BytePos::from_usize(range.end),
+        SyntaxContext::root(),
+        None,
+    )
 }
 
-struct CargoToml {}
+#[derive(Deserialize, Debug)]
+pub(crate) struct CargoToml {
+    dependencies: BTreeMap<Spanned<String>, Spanned<toml::Value>>,
+}
